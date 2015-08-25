dfc1c7e2c4f277dd056f055bba8877106e122364
bmd <brian@murphydye.com>
2015-08-24 20:35:53 -0600

    New expectations.

--- lib/helpr.rb
+++ lib/helpr.rb
@@ -3,5 +3,26 @@ require_relative 'helpr/version'
 module Helpr
   class Help
     attr_accessor :top
+    
+    def add(name, text)
+      self[name.to_sym] = text
+      tokens = name.split(' ')
+    end
+
+    def [](name)
+      help[name]
+    end
+
+    def []=(name, val)
+      help[name] = val
+    end
+
+    def help
+      @help ||= Hash.new
+    end
+
+    def topic_hierarchy
+
+    end
   end
 end
--- spec/helpr_spec.rb
+++ spec/helpr_spec.rb
@@ -1,8 +1,15 @@
 require_relative 'spec_helper'
 
+#     call a module method without including it:
+# Xyz.instance_method(:c).bind(self).call
+#
+#     what this needs though is this:
+# z = Class.new.send(:include, Xyz).new
+# z.c    OR   z.send(:c)
+
 module TestHelp
-  def top
-    'help on test topic'
+  def main
+    "help main. Topics include: #{self.topics}"
   end
 
   def test1
@@ -14,6 +21,20 @@ module TestHelp
   end
 end
 
+module TestHelp2
+  def main
+    "help2 main. Topics include: #{self.topics}"
+  end
+
+  def test1
+    'help2 for test1'
+  end
+
+  def test2
+    'help2 for test2'
+  end
+end
+
 describe Helpr::Help do
   let(:help) { Helpr::Help.new }
 
@@ -22,11 +43,34 @@ describe Helpr::Help do
     expect(help.top).to eq('top help')
   end
 
-  xit 'is the dream' do
-    help.add TestModule
-    expect(help.topics).to eq('test')
-    expect(help['test']).to eq('help for test topic')
-    expect(help['test test2']).to eq('help on test2')
-    expect(help['']).to eq("top help\n\nTopics:\n- test")
+  context 'after adding topics' do
+    before(:each) {
+      help.add('test 1', 'this is test 1')
+      help.add('test 2', 'this is test 2')
+      help.add('note 1', 'this is note 1')
+      help.add('note 2', 'this is note 2')
+      help.add('note 3', 'this is note 3')
+    }
+
+    it 'should have test 1' do
+      expect(help['test 1']).to eq('this is test 1')
+    end
+
+    xit 'should have test 2' do
+      expect(help['test 2']).to eq('this is test 2')
+    end
+
+    xit 'should have test 2 as a symbol' do
+      expect(help[:'test 2']).to eq('this is test 2')
+    end
+
+    xit 'should have two topics' do
+      expect(help.topics).to eq(['test', 'note'])
+    end
+
+    xit 'note should have three subtopics' do
+      expect(help['note'].topics).to eq(['note 1', 'note 2', 'note 3'])
+      expect(help[:note].topics).to eq(['note 1', 'note 2', 'note 3'])
+    end
   end
 end
